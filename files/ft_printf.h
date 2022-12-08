b2733eb40f0ed939fcda82d8084a8080fd7d1faf
CRESPY Arthur <arthur@crespy.eu>
2022-12-08 17:00:47 +0100

    Updates prototypes

--- files/ft_printf.h
+++ files/ft_printf.h
@@ -6,17 +6,23 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:17:42 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/28 12:49:11 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/11/28 13:37:17 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <malloc.h>
 
-int     ft_printf(const char *str, ...);
-void ft_putstr(char *str);
-void ft_putchar(char c);
+void	ft_printf(const char *str, ...);
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
+void	ft_putadr(char *adr);
+void	ft_putnbr(long long n);
+char	*ft_itoa(int n);
+void     ft_print(const char *str, va_list args);
 #endif
