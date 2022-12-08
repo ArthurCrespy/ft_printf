41f64d9b1500bac1a01fb125a0205cb3acfa2d09
CRESPY Arthur <arthur@crespy.eu>
2022-12-08 17:00:59 +0100

    Add new utils functions

--- files/ft_printf_utils.c
+++ files/ft_printf_utils.c
@@ -6,17 +6,13 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 12:49:07 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/28 12:49:09 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/11/28 13:37:04 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-// Created by acrespy on 11/28/22.
-//
-
 #include "ft_printf.h"
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	while (str && *str != '\0')
 	{
@@ -25,7 +21,42 @@ void ft_putstr(char *str)
 	}
 }
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
+void	ft_putadr(char *adr)
+{
+	write(1, adr, 14);
+}
+
+void	ft_putnbr(long long n)
+{
+	long long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + 48);
+}
+size_t	ft_len(int n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
