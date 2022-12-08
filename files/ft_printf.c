baee4a439741b891393e6b985216d4991d3f4b48
CRESPY Arthur <arthur@crespy.eu>
2022-12-08 17:00:37 +0100

    Implement new conversions

--- files/ft_printf.c
+++ files/ft_printf.c
@@ -6,35 +6,50 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:17:24 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/28 12:49:18 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/11/28 13:08:25 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int     ft_printf(const char *str, ...)
+void	ft_printf(const char *str, ...)
 {
-	int i;
-	char *s;
-	va_list args;
+	va_list	args;
 
-	i = 0;
-	s = (char *) str;
 	va_start(args, str);
-
 	while (str && *str != '\0')
 	{
 		if (*str == '%')
 		{
 			str++;
-			if (*str == '%')
-				ft_putchar(*str);
-			if (*str == 'c')
-				ft_putchar(va_arg(args, int));
+			ft_print(str, args);
 		}
 		else
 			ft_putchar(*str);
 		str++;
 	}
-	return (0);
+	va_end(args);
+}
+
+void     ft_print(const char *str, va_list args)
+{
+		if (*str == '%')
+			ft_putchar(*str);
+		if (*str == 'c')
+			ft_putchar(va_arg(args, int));
+		if (*str == 's')
+			ft_putstr(va_arg(args, char *));
+		if (*str == 'p')
+		{
+			ft_putstr(va_arg(args, void *));
+		}
+		if (*str == 'd')
+			ft_putnbr(va_arg(args, double));
+		if (*str == 'i')
+			ft_putnbr(va_arg(args, int));
+		if (*str == 'u')
+			ft_putnbr(va_arg(args, int));
+		if (*str == 'x')
+			ft_putnbr(va_arg(args, int));
+		str++;
 }
