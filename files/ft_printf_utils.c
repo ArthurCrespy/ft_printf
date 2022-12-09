d0c0be5d5f1d01a6d4f3b0716437a393dce72ece
CRESPY Arthur <arthur@crespy.eu>
2022-12-09 13:21:49 +0100

    Add utils file

--- files/ft_printf_utils.c
+++ files/ft_printf_utils.c
@@ -1,62 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/28 12:49:07 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/28 13:37:04 by acrespy          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//
+// Created by acrespy on 12/9/22.
+//
 
 #include "ft_printf.h"
 
-void	ft_putstr(char *str)
+int	ft_strlen(char *str)
 {
-	while (str && *str != '\0')
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	check_base(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (ft_strlen(str) <= 1)
+		return (0);
+	while (str[i] != 0x00)
 	{
-		write(1, str, 1);
-		str++;
+		if (!(str[i] >= 65 && str[i] <= 90)
+		    && !(str[i] >= 97 && str[i] <= 122)
+		    && !(str[i] >= 48 && str[i] <= 57))
+			return (0);
+		j = i + 1;
+		while (str[j])
+		{
+			if (str[j] == str[i])
+				return (0);
+			j++;
+		}
+		i++;
 	}
+	return (1);
 }
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putadr(char *adr)
-{
-	write(1, adr, 14);
-}
-
-void	ft_putnbr(long long n)
-{
-	long long	nb;
-
-	nb = n;
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb *= -1;
-	}
-	if (nb > 9)
-		ft_putnbr(nb / 10);
-	ft_putchar(nb % 10 + 48);
-}
-size_t	ft_len(int n)
-{
-	size_t	len;
-
-	len = 0;
-	if (n <= 0)
-		len++;
-	while (n)
-	{
-		len++;
-		n /= 10;
-	}
-	return (len);
-}
-
