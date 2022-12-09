b250e07ea276e569f6be02a0c9450303e6ef8fa9
CRESPY Arthur <arthur@crespy.eu>
2022-12-09 13:39:22 +0100

    Update main.c

--- files/main.c
+++ files/main.c
@@ -5,10 +5,11 @@ int	main(void)
 	char	c = 'a';
 	char	*s = "Bienvenue a 42";
 	void    *p = "Ouistiti";
-	float nb = 24.5;
+	double  nb = 24.5;
 	int     nb1 = -9221;
+	int     nb2 = 654653165;
 
 	printf("\n> off funct -> %f\n", nb);
-	ft_printf("\n> %% -> %% | char -> %c | str - > %s | address : %p | nb -> %d | unb -> %i\n\n", c, s, p, nb, nb1);
+	ft_printf("\n> %% -> %% | char -> %c | str - > %s | address : %p | nb -> %d | nb -> %i | unb -> %u | hexa -> %x | HEXA -> %X\n\n", c, s, p, nb, nb1, nb1, nb2, nb2);
 	return (0);
 }
