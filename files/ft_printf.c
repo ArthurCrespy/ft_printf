f95cabd9c546b9387856d43e19ae2dc47f0727ba
CRESPY Arthur <arthur@crespy.eu>
2022-12-10 11:44:58 +0100

    Modify prototype from void to int and add additional utils functions

--- files/ft_printf.c
+++ files/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:17:24 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/28 13:08:25 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/12/09 17:35:09 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_printf(const char *str, ...)
+int	ft_printf(const char *str, ...)
 {
 	va_list	args;
 
@@ -25,34 +25,55 @@ void	ft_printf(const char *str, ...)
 			ft_print(str, args);
 		}
 		else
+		{
 			ft_putchar(*str);
+		}
 		str++;
 	}
 	va_end(args);
+	return (ft_count_print(0));
 }
 
-void     ft_print(const char *str, va_list args)
+void	ft_print(const char *str, va_list args)
 {
-		if (*str == 'c')
-			ft_putchar(va_arg(args, int));
-		if (*str == 's')
-			ft_putstr(va_arg(args, char *));
-		if (*str == 'p')
-		{
-			ft_putstr("0x");
-			ft_putnbr_base(va_arg(args, long), "0123456789abcdef");
-		}
-		if (*str == 'd')
-			ft_putnbr(va_arg(args, double), 1);
-		if (*str == 'i')
-			ft_putnbr(va_arg(args, int), 1);
-		if (*str == 'u')
-			ft_putnbr(va_arg(args, int), 0);
-		if (*str == 'x')
-			ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
-		if (*str == 'X')
-			ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF");
-		if (*str == '%')
-			ft_putchar(*str);
-		str++;
+	if (*str == 'c')
+		ft_putchar(va_arg(args, int));
+	if (*str == 's')
+		ft_putstr(va_arg(args, char *));
+	if (*str == 'p')
+	{
+		ft_putstr("0x");
+		ft_putnbr_base(va_arg(args, long long), "0123456789abcdef", 0);
+	}
+	if (*str == 'd')
+		ft_putnbr(va_arg(args, int), 1);
+	if (*str == 'i')
+		ft_putnbr(va_arg(args, int), 1);
+	if (*str == 'u')
+		ft_putnbr(va_arg(args, int), 0);
+	if (*str == 'x')
+		ft_putnbr_base(va_arg(args, int), "0123456789abcdef", 0);
+	if (*str == 'X')
+		ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF", 0);
+	if (*str == '%')
+		ft_putchar(*str);
+	str++;
+}
+
+int	ft_count_print(int count)
+{
+	static int	i;
+
+	i += count;
+	return (i);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
