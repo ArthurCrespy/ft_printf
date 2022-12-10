1c735f1f452cff4d90cc8fe1b8500582ad897fdb
CRESPY Arthur <arthur@crespy.eu>
2022-12-10 20:07:24 +0100

    Review tests in main.c

--- files/main.c
+++ files/main.c
@@ -16,33 +16,21 @@ int	main(void)
 {
 	int		count_char = 0;
 	int		off_count_char = 0;
-/*	char	c = 'a';
+	char	c = 'a';
 	char	*s = NULL;
 	char	*o = "Ouistiti";
 	void	*p = &o;
 	int		nb = 5165165;
-	long		nb1 = 2147483647;
+	long	nb1 = 2147483647;
 	int		nb2 = 654653165;
-	long    *l = 2147483647;
-	long    *ll = &l;*/
-    long     d = 0;
 
-	/*printf("\n---- OFFICIAL FUNCTION ----");
+	printf("\n---- OFFICIAL FUNCTION ----");
 	off_count_char = printf("\n> %% -> %% | char -> %c | str - > %s | address : %p | nb -> %d | nb -> %i | unb -> %u | hexa -> %x | HEXA -> %X\n", c, s, p, nb, nb1, nb1, nb2, nb2);
 	printf("\n> nb of characters printed -> %d\n\n", off_count_char);
 
-	ft_printf("\n---- ACRESPY's FUNCTION ----");
+	printf("\n---- ACRESPY's FUNCTION ----");
 	count_char = ft_printf("\n> %% -> %% | char -> %c | str - > %s | address : %p | nb -> %d | nb -> %i | unb -> %u | hexa -> %x | HEXA -> %X\n", c, s, p, nb, nb1, nb1, nb2, nb2);
-	ft_printf("\n> nb of characters printed -> %d\n\n", count_char);
-
-	ft_printf("\n----%%P TEST----");*/
-
-	printf("\n---- OFFICIAL FUNCTION ----\n> ");
-	off_count_char = printf("%p", d);
-	printf("\n> nb of characters printed -> %d\n\n", off_count_char);
-
-	printf("\n---- ACRESPY's FUNCTION ----\n >");
-	count_char = ft_printf("%p", d);
 	printf("\n> nb of characters printed -> %d\n\n", count_char);
+
 	return (0);
 }
