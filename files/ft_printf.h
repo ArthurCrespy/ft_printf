460c2f4ef52c9147f5ce054156b53d36863f8807
CRESPY Arthur <arthur@crespy.eu>
2022-12-10 11:45:27 +0100

    Update prototypes ans fix Normiette

--- files/ft_printf.h
+++ files/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:17:42 by acrespy           #+#    #+#             */
-/*   Updated: 2022/11/28 13:37:17 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/12/09 17:35:36 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdarg.h>
-# include <malloc.h>
 # include <stdlib.h>
+# include <limits.h>
 
-void	ft_printf(const char *str, ...);
-void    ft_print(const char *str, va_list args);
+int		ft_printf(const char *str, ...);
+void	ft_print(const char *str, va_list args);
 void	ft_putstr(char *str);
 void	ft_putchar(char c);
 void	ft_putnbr(long long n, int method);
-int     ft_strlen(char *str);
-void	ft_putnbr_base(long nbr, char *base);
-int     check_base(char *str);
+int		ft_strlen(char *str);
+int		ft_count_print(int count);
+void ft_putnbr_base(long long nbr, char *base, int check);
 #endif
