b2d801db0de0929323ee4f8eb0851e4c025944a8
CRESPY Arthur <arthur@crespy.eu>
2022-12-13 17:10:46 +0100

    Fix Norminette issues and modify ft_count_print() prototype

--- files/ft_printf.h
+++ files/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 14:17:42 by acrespy           #+#    #+#             */
-/*   Updated: 2022/12/09 17:35:36 by acrespy          ###   ########.fr       */
+/*   Updated: 2022/12/12 17:19:32 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ int		ft_printf(const char *str, ...);
 void	ft_print(const char *str, va_list args);
 void	ft_putstr(char *str);
 void	ft_putchar(char c);
-void    ft_putadr(long long nbr);
-void ft_putnbr(long long n);
+void	ft_putadr(long long nbr);
+void	ft_putnbr(long long n);
 int		ft_strlen(char *str);
+int		ft_count_print(int count, int reset);
+void	ft_putnbr_base(long long nbr, char *base);
 int		ft_count_print(int count);
 void    ft_putnbr_base(long long nbr, char *base);
 #endif
