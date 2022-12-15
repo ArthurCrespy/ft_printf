bcd6165daf31d62205e83dabda417e4fe900ef67
CRESPY Arthur <arthur@crespy.eu>
2022-12-15 16:08:24 +0100

    Modify prototype from longlong to unsigned long

--- files/ft_printf.h
+++ files/ft_printf.h
@@ -23,11 +23,11 @@ int		ft_printf(const char *str, ...);
 void	ft_print(const char *str, va_list args);
 void	ft_putstr(char *str);
 void	ft_putchar(char c);
-void	ft_putadr(long long nbr);
+void	ft_putadr(unsigned long nbr);
 void	ft_putnbr(long long n);
 int		ft_strlen(char *str);
 int		ft_count_print(int count, int reset);
-void	ft_putnbr_base(long long nbr, char *base);
-int		ft_count_print(int count);
-void    ft_putnbr_base(long long nbr, char *base);
+void	ft_putnbr_base(unsigned long nbr, char *base);
+int		ft_count_print(int count, int reset);
+void    ft_putnbr_base(unsigned long nbr, char *base);
 #endif
